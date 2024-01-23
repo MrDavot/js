dfae23905f2b22250e8eca859aafc862470485ac
MrDavot <36482903+MrDavot@users.noreply.github.com>
2024-01-23 12:10:32 +0300

    Add files via upload

---
+++ vs1/main.js
@@ -0,0 +1,92 @@
+//Работа с переменными
+
+//var num = 3;
+//alert(num);
+
+//var a = 10;
+//var b = 2;
+//alert("Сумма: " + (a + b) + "\nРазность: " + (a - b) + "\nПроизведение: " + (a * b) + "\nЧастное: " + (a / b));
+
+//var c = 15;
+//var d = 2;
+//var result = c + d;
+//alert(result);
+
+//var a = 10;
+//var b = 2;
+//var c = 5;
+//alert("Сумма: " + (a + b + c));
+
+//var a = 17;
+//var b = 10;
+//var c = a - b;
+//var d = 7;
+//var result = c + d;
+//alert(result);
+
+//Работа со строками
+
+//var str = 'Привет, Мир!';
+//alert(str);
+
+//var str1 = 'Привет, ';
+//var str2 = 'Мир!';
+
+//alert(str1 + str2);
+//var name = 'Ваше Имя';
+//alert('Привет, ' + name + '!');
+
+//var age = 25;
+//alert('Мне ' + age + ' лет!');   
+
+//Функция prompt
+
+//let name = prompt("What is your name?");
+//alert("Your name is " + name);
+
+//let number = prompt("Enter a number");
+//alert(number * number);
+
+//Обращение к символам строки
+
+//let str = 'abcde';
+//console.log(str.charAt(0));
+//console.log(str.charAt(2));
+//console.log(str.charAt(4));
+
+//let num = '12345';
+//let product = 1;
+//for (let i = 0; i < num.length; i++) {
+// product *= parseInt(num.charAt(i));
+//}
+//console.log(product);
+
+//Практика
+
+//let hour = 60 * 60;
+//let day = 24 * hour;
+//let month = 30 * day;
+
+//let h = 10;
+//let m = 35;
+//let s = 5;
+//console.log(`Текущее время: ${h}:${m}:${s}`);
+
+//let num = 5;
+//let square = num * num;
+//console.log(`Квадрат числа ${num} равен ${square}`);
+
+//Работа с присваиванием и декрементами
+
+//let num = 47;
+//num += 7;
+//num -= 18;
+//num *= 10;
+//num /= 15;
+//console.log(num);
+
+let num = 10;
+num += 1;
+num += 1;
+num--;
+alert(num);
